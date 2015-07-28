d5a775c61fee58a1fd09c7ef3bbfd965ac9188d5
Marko Vojvodic <marko.vojvodic@mongodb.com>
2015-07-28 14:56:46 -0400

    Fixed bug when closing collection factory client

--- src/main/java/com/mongodb/spark/MongoSparkCollectionFactory.java
+++ src/main/java/com/mongodb/spark/MongoSparkCollectionFactory.java
@@ -74,5 +74,6 @@ public class MongoSparkCollectionFactory<T> implements MongoCollectionFactory<T>
     @Override
     public void closeClient() throws IOException {
         this.clientFactory.close();
+        this.mongoCollection = null;
     }
 }
