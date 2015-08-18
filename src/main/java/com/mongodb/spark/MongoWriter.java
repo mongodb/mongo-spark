44ce6a19d308af94fc2007ef89f4680b5d0ac882
Marko Vojvodic <marko.vojvodic@mongodb.com>
2015-08-18 11:36:51 -0400

    Broadcast collection provider in MongoWriter

--- src/main/java/com/mongodb/spark/MongoWriter.java
+++ src/main/java/com/mongodb/spark/MongoWriter.java
@@ -21,11 +21,13 @@ import com.mongodb.client.model.InsertOneModel;
 import com.mongodb.client.model.ReplaceOneModel;
 import com.mongodb.client.model.UpdateOptions;
 import com.mongodb.client.model.WriteModel;
+import org.apache.spark.broadcast.Broadcast;
 import org.apache.spark.rdd.RDD;
 import org.bson.Document;
 import org.bson.codecs.Codec;
 import org.bson.codecs.CollectibleCodec;
 import scala.collection.Iterator;
+import scala.reflect.ClassTag$;
 import scala.runtime.BoxedUnit;
 
 import java.util.ArrayList;
@@ -36,23 +38,32 @@ import java.util.List;
  */
 public final class MongoWriter {
     /**
-     * Writes the given RDD to the collection specified in the collection factory.
+     * Writes the given RDD to the collection specified in the collection provider.
      * If there is a CollectibleCodec available for the documents and document ids
      * exist, the writer will attempt to replace documents in the specified
      * collection with documents in the RDD with the provided upsert option.
      *
+     * Note: the collection provider will be broadcasted through the RDD's SparkContext.
+     *
      * @param rdd the rdd to write
-     * @param factory the collection factory
+     * @param provider the collection provider
      * @param upsert true if upsert
      * @param ordered true if ordered
      * @param <T> the type of documents in the collection
      */
-    public static <T> void writeToMongo(final RDD<T> rdd, final MongoCollectionProvider<T> factory, final Boolean upsert,
+    public static <T> void writeToMongo(final RDD<T> rdd, final MongoCollectionProvider<T> provider, final Boolean upsert,
                                         final Boolean ordered) {
+        Broadcast<MongoCollectionProvider<T>> collectionProvider =
+                rdd.sparkContext().broadcast(provider, ClassTag$.MODULE$.apply(provider.getClass()));
+
         rdd.foreachPartition(new SerializableAbstractFunction1<Iterator<T>, BoxedUnit>() {
             @Override
             public BoxedUnit apply(final Iterator<T> elements) {
-                Codec<T> codec = factory.getCollection().getCodecRegistry().get(factory.getCollection().getDocumentClass());
+                Codec<T> codec = collectionProvider.value()
+                                                   .getCollection()
+                                                   .getCodecRegistry().get(collectionProvider.value()
+                                                                                             .getCollection()
+                                                                                             .getDocumentClass());
                 boolean isCollectibleCodec = codec instanceof CollectibleCodec;
                 List<WriteModel<T>> writeModels = new ArrayList<>();
                 T element;
@@ -61,14 +72,14 @@ public final class MongoWriter {
                     element = elements.next();
                     if (isCollectibleCodec && ((CollectibleCodec<T>) codec).documentHasId(element)) {
                         writeModels.add(new ReplaceOneModel<>(new Document("_id", ((CollectibleCodec<T>) codec).getDocumentId(element)),
-                                        element, new UpdateOptions().upsert(upsert)));
+                                                              element, new UpdateOptions().upsert(upsert)));
                     } else {
                         writeModels.add(new InsertOneModel<>(element));
                     }
                 }
 
                 if (writeModels.size() > 0) {
-                    factory.getCollection().bulkWrite(writeModels, new BulkWriteOptions().ordered(ordered));
+                    collectionProvider.value().getCollection().bulkWrite(writeModels, new BulkWriteOptions().ordered(ordered));
                 }
 
                 return null;
