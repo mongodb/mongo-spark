2138a5d934f9f99e053d109e5ac9ba54526edf32
Ross Lawley <ross.lawley@gmail.com>
2018-07-04 11:10:50 +0100

    Ensure nested maps are also handled
    SPARK-133

--- src/main/scala/com/mongodb/spark/sql/MongoInferSchema.scala
+++ src/main/scala/com/mongodb/spark/sql/MongoInferSchema.scala
@@ -77,7 +77,10 @@ object MongoInferSchema extends Logging {
     // perform schema inference on each row and merge afterwards
     val rootType: DataType = sampleData
       .map(getSchemaFromDocument(_, mongoRDD.readConfig))
-      .treeAggregate[DataType](StructType(Seq()))(compatibleType(_, _, mongoRDD.readConfig), compatibleType(_, _, mongoRDD.readConfig))
+      .treeAggregate[DataType](StructType(Seq()))(
+        compatibleType(_, _, mongoRDD.readConfig, nested = false),
+        compatibleType(_, _, mongoRDD.readConfig, nested = false)
+      )
     canonicalizeType(rootType) match {
       case Some(st: StructType) => st
       case _                    => StructType(Seq()) // canonicalizeType erases all empty structs, including the only one we want to keep
@@ -131,10 +134,12 @@ object MongoInferSchema extends Logging {
    *
    * @param t1 the DataType of the first element
    * @param t2 the DataType of the second element
+   * @param readConfig the readConfig
+   * @param nested true if comparing nested values
    * @return the DataType that matches on the input DataTypes
    */
-  private def compatibleType(t1: DataType, t2: DataType, readConfig: ReadConfig): DataType = {
-    TypeCoercion.findTightestCommonType(t1, t2).getOrElse {
+  private def compatibleType(t1: DataType, t2: DataType, readConfig: ReadConfig, nested: Boolean = true): DataType = {
+    val dataType = TypeCoercion.findTightestCommonType(t1, t2).getOrElse {
       // t1 or t2 is a StructType, ArrayType, or an unexpected type.
       (t1, t2) match {
         // Double support larger range than fixed decimal, DecimalType.Maximum should be enough
@@ -152,9 +157,7 @@ object MongoInferSchema extends Logging {
             DecimalType(range + scale, scale)
           }
 
-        case (StructType(fields1), StructType(fields2)) =>
-          structsToMapType(fields1, fields2, readConfig) getOrElse compatibleStructType(fields1, fields2, readConfig)
-
+        case (StructType(fields1), StructType(fields2)) => compatibleStructType(fields1, fields2, readConfig)
         case (MapType(keyType1, valueType1, valueContainsNull1), MapType(keyType2, valueType2, valueContainsNull2)) =>
           MapType(
             compatibleType(keyType1, keyType2, readConfig),
@@ -182,6 +185,10 @@ object MongoInferSchema extends Logging {
         case (_, _) => ConflictType
       }
     }
+    dataType match {
+      case structType: StructType if nested => structTypeToMapType(structType, readConfig)
+      case _                                => dataType
+    }
   }
   // scalastyle:on cyclomatic.complexity method.length
 
@@ -201,28 +208,16 @@ object MongoInferSchema extends Logging {
     StructType(newFields.toSeq.sortBy(_.name))
   }
 
-  /**
-   * Tries to combine the fields of two struct types to a MapType.
-   *
-   * This method returns Some if a MapType could be generated or None if it couldn't.
-   * It will only try to create a MapType if the minimum number of keys over both structs has been reached.
-   * All fields will be iterated and combined to find a compatible value type.
-   *
-   * @param fields1 the fields of the first struct
-   * @param fields2 the fields of the second struct
-   * @return the generated MapType
-   */
-  private def structsToMapType(fields1: Array[StructField], fields2: Array[StructField], readConfig: ReadConfig): Option[MapType] = {
-    val fieldNames = (fields1 ++ fields2).map(_.name).distinct
-    if (readConfig.schemaInferMapTypesEnabled && fieldNames.length >= readConfig.schemaInferMapTypesMinimumKeys) {
-      (fields1 ++ fields2).map(_.dataType).reduce(compatibleType(_, _, readConfig)) match {
-        case ConflictType       => None
-        case SkipFieldType      => None
-        case dataType: DataType => Some(DataTypes.createMapType(StringType, dataType, true))
-        case _                  => None
+  private def structTypeToMapType(structType: StructType, readConfig: ReadConfig): DataType = {
+    if (readConfig.schemaInferMapTypesEnabled && structType.fields.length >= readConfig.schemaInferMapTypesMinimumKeys) {
+      structType.fields.map(_.dataType).reduce(compatibleType(_, _, readConfig)) match {
+        case ConflictType       => structType // ignore conflicting types
+        case SkipFieldType      => structType // ignore skipfield types
+        case dataType: DataType => DataTypes.createMapType(StringType, dataType, true)
+        case _                  => structType
       }
     } else {
-      None
+      structType
     }
   }
 
@@ -241,8 +236,6 @@ object MongoInferSchema extends Logging {
   val compatibleDecimalTypes = Seq(ByteType, ShortType, IntegerType, LongType, FloatType, DoubleType)
 
   // scalastyle:off magic.number
-  // The minimum number of keys after which a MapType can be detected
-  private val minimumMapKeys = 250
   // The decimal types compatible with other numeric types
   private val ByteDecimal = DecimalType(3, 0)
   private val ShortDecimal = DecimalType(5, 0)
@@ -340,7 +333,7 @@ object MongoInferSchema extends Logging {
         val previous: Option[DataType] = arrayType
         arrayType = Some(getDataType(bsonValue, readConfig))
         if (previous.nonEmpty && arrayType != previous) arrayType = Some(compatibleType(arrayType.get, previous.get, readConfig))
-        arrayType != Some(ConflictType) // Option.contains was added in Scala 2.11
+        !arrayType.contains(ConflictType)
     })
     arrayType.get match {
       case SkipFieldType => SkipFieldType
