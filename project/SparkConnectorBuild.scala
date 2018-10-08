d47417109a61206cf4e4189dc3cdd6b067b85c7c
Ross Lawley <ross.lawley@gmail.com>
2018-10-08 12:17:23 +0100

    Build: Version 2.1.4

--- project/SparkConnectorBuild.scala
+++ project/SparkConnectorBuild.scala
@@ -30,7 +30,7 @@ object SparkConnectorBuild extends Build {
   val buildSettings = Seq(
     organization := "org.mongodb.spark",
     organizationHomepage := Some(url("http://www.mongodb.org")),
-    version := "2.1.4-SNAPSHOT",
+    version := "2.1.4",
     scalaVersion := scalaCoreVersion,
     crossScalaVersions := scalaVersions,
     libraryDependencies ++= coreDependencies,
