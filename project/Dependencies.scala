5b6d3cd0c3dc7bac67c990fbaa1a0f2bf03f2581
Ross Lawley <ross.lawley@gmail.com>
2018-11-20 15:53:49 +0000

    Build: Updated dependency to Spark 2.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   val scalaCoreVersion        = "2.11.12"
   val scalaVersions           = Seq("2.11.12")
   val mongodbDriverVersion    = "3.8.2"
-  val sparkVersion            = "2.3.2"
+  val sparkVersion            = "2.4.0"
   val slf4jVersion            = "1.7.16"
 
   val scalaTestVersion        = "2.2.4"
