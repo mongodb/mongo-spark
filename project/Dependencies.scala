88e0a720b3a59e63d3c608bd1b3bae3ead67a263
Ross Lawley <ross.lawley@gmail.com>
2020-06-03 08:40:48 +0100

    Updated Java driver to 3.12.5
    SPARK-265

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
   val scalaCoreVersion        = "2.11.12"
 
   val scalaVersions           = Seq("2.11.12", "2.10.7")
-  val mongodbDriverVersion    = "3.12.4"
+  val mongodbDriverVersion    = "3.12.5"
   val sparkVersion            = "2.2.2"
   val slf4jVersion            = "1.7.16"
 
