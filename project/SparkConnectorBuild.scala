8e876a71c383a8eff6dd1c87ab97cbecd7d99477
Ross Lawley <ross.lawley@gmail.com>
2020-06-04 09:05:52 +0100

    Version: 2.1.8

--- project/SparkConnectorBuild.scala
+++ project/SparkConnectorBuild.scala
@@ -29,7 +29,7 @@ object SparkConnectorBuild extends Build {
   import Dependencies.{scalaCoreVersion, scalaVersions, coreDependencies, testDependencies}
   import Resolvers._
 
-  val baseVersion = "2.1.7"
+  val baseVersion = "2.1.8"
 
   val buildSettings = Seq(
     organization := "org.mongodb.spark",
