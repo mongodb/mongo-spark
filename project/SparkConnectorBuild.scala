4f924f62e74a21addb8ddbd57135a78b228d6ea2
Ross Lawley <ross.lawley@gmail.com>
2021-02-03 10:16:00 +0000

    Build: Version baseline 2.3.6

--- project/SparkConnectorBuild.scala
+++ project/SparkConnectorBuild.scala
@@ -29,7 +29,7 @@ object SparkConnectorBuild extends Build {
   import Dependencies.{scalaCoreVersion, scalaVersions, coreDependencies, testDependencies}
   import Resolvers._
 
-  val baseVersion = "2.3.5"
+  val baseVersion = "2.3.6"
 
   val buildSettings = Seq(
     organization := "org.mongodb.spark",
