23fd06f10b4203b00c0dda7b10d05c9b7115043b
Ross Lawley <ross.lawley@gmail.com>
2022-01-21 14:02:19 +0000

    Remove unused imports

--- src/main/java/com/mongodb/spark/sql/connector/config/MongoConfig.java
+++ src/main/java/com/mongodb/spark/sql/connector/config/MongoConfig.java
@@ -25,7 +25,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.jetbrains.annotations.ApiStatus;
 
