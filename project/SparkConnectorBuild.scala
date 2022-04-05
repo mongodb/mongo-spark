f614ba0954ded6e8f0e20374fe7e6ba78b28afc1
Ross Lawley <ross.lawley@gmail.com>
2022-04-05 17:02:23 +0100

    Revert back to Version 2.2.10

--- project/SparkConnectorBuild.scala
+++ project/SparkConnectorBuild.scala
@@ -28,7 +28,7 @@ object SparkConnectorBuild extends Build {
   import Dependencies.{scalaCoreVersion, scalaVersions, coreDependencies, testDependencies}
   import Resolvers._
 
-  val baseVersion = "2.2.11"
+  val baseVersion = "2.2.10"
 
   val buildSettings = Seq(
     organization := "org.mongodb.spark",
