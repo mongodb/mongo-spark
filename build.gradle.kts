7f049b3b9b62faec66f04c19cb437e9287b00a69
Ross Lawley <ross.lawley@gmail.com>
2022-10-12 14:16:00 +0100

    Version: 10.1.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ plugins {
 }
 
 group = "org.mongodb.spark"
-version = "10.0.5-SNAPSHOT"
+version = "10.1.0-SNAPSHOT"
 description = "The official MongoDB Apache Spark Connect Connector."
 
 java {
