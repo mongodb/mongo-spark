ea2995acea56567d069921545e660d61e01bd666
Ross Lawley <ross.lawley@gmail.com>
2022-11-07 15:04:50 +0000

    Return a distinct list of preferred locations

--- src/main/java/com/mongodb/spark/sql/connector/read/partitioner/PartitionerHelper.java
+++ src/main/java/com/mongodb/spark/sql/connector/read/partitioner/PartitionerHelper.java
@@ -161,6 +161,7 @@ public final class PartitionerHelper {
         .getServerDescriptions()
         .stream()
         .flatMap(sd -> sd.getHosts().stream())
+        .distinct()
         .collect(Collectors.toList());
   }
 
