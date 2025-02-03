e3cfe1252980026765268d5122fc35f798261620
Tomasz Zarna <tzarna@gmail.com>
2025-02-03 16:40:56 +0100

    Update tests in MongoClientCacheTest (#129)
    Removed duplicated test
    
    SPARK-441
    Co-authored-by: Ross Lawley <ross@mongodb.com>

--- src/test/java/com/mongodb/spark/sql/connector/connection/MongoClientCacheTest.java
+++ src/test/java/com/mongodb/spark/sql/connector/connection/MongoClientCacheTest.java
@@ -126,19 +126,6 @@ class MongoClientCacheTest {
   }
 
   @Test
-  void factoriesWithSameSimpleConfigCreateSameClientsThroughCache() {
-    MongoConfig config = MongoConfig.createConfig(CONFIG_MAP);
-    DefaultMongoClientFactory factory1 = new DefaultMongoClientFactory(config);
-    DefaultMongoClientFactory factory2 = new DefaultMongoClientFactory(config);
-    MongoClientCache mongoClientCache = new MongoClientCache(0, 0, 100);
-
-    MongoClient client1 = mongoClientCache.acquire(factory1);
-    MongoClient client2 = mongoClientCache.acquire(factory2);
-
-    assertSame(client1, client2);
-  }
-
-  @Test
   void factoriesWithEqualSimpleConfigsCreateSameClientsThroughCache() {
     MongoConfig config1 = MongoConfig.createConfig(CONFIG_MAP);
     MongoConfig config2 = MongoConfig.createConfig(CONFIG_MAP);
