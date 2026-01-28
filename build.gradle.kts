e01d8990fb5936b6e41acc33ddae65ea87dda0c5
Ross Lawley <ross.lawley@gmail.com>
2026-01-28 18:13:53 +0000

    Update shadowjar plugin to work with latest gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ plugins {
     id("com.github.gmazzo.buildconfig") version "3.0.2"
     id("com.github.spotbugs") version "6.4.2"
     id("com.diffplug.spotless") version "6.19.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.3.1"
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
